477d9f8daba2bac1217cc696ac8c437ba80fcf36
Dacron <favouradebimpe63@gmail.com>
2024-02-12 23:34:06 +0100

    adjusted link length on mobile

--- src/Components/Unique.jsx
+++ src/Components/Unique.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Stack, Typography, IconButton, Tooltip } from "@mui/material";
+import { Stack, Typography, IconButton, Tooltip, Box } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -13,11 +13,27 @@ function Unique() {
     }, 1500);
   };
   const uniqueLink = localStorage.getItem("uniqueLink");
+
+  const breakLongURL = (url) => {
+    const maxLength = 40; // Set the maximum length for each line
+    const parts = [];
+    for (let i = 0; i < url.length; i += maxLength) {
+      parts.push(url.substring(i, i + maxLength));
+    }
+    return parts.join("\n");
+  };
+
   return (
     <Stack spacing={2}>
-      <Typography>Your unique link is:</Typography>
-      <Stack direction="row" sx={{ alignItems: "center" }} spacing={2}>
-        <Typography>{uniqueLink}</Typography>
+      <Typography variant="h6">
+        <b>Your unique link is:</b>
+      </Typography>
+
+      <Stack direction="row" spacing={1.2} sx={{ alignItems: "center" }}>
+        
+        <Box>
+          <Typography> {breakLongURL(uniqueLink)}</Typography>
+        </Box>
         {!copied && (
           <CopyToClipboard text={uniqueLink} onCopy={handleCopy}>
             <IconButton>
